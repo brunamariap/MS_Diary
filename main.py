b904fd1dee33248588ca259173608fa654b51f9b
Bruna Maria <bruna.mariap@outlook.com>
2023-11-07 21:53:58 -0300

    Criados testes do CRUD de Diary

--- main.py
+++ main.py
@@ -1,24 +1,12 @@
 from fastapi import FastAPI
 from prisma import Prisma
 from controllers import grade, diary
-from contextlib import asynccontextmanager, contextmanager
 
 app = FastAPI()
 app.include_router(grade.router)
 app.include_router(diary.router)
 
 prisma = Prisma(auto_register=True)
-
-# @asynccontextmanager
-# async def lifespan(app: FastAPI):
-#     # Load the ML model
-#     # ml_models["answer_to_everything"] = fake_answer_to_everything_ml_model
-#     await prisma.connect()
-#     yield
-#     await prisma.disconnect()
-#     # Clean up the ML models and release the resources
-#     # ml_models.clear()
-
 
 @app.on_event("startup")
 def startup():
